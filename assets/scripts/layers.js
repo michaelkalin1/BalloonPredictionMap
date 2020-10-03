f7bf66528520acc9ca59d5d728c775ea0c98c81a
Zach <zrb@umd.edu>
2020-10-03 15:03:30 -0400

    fix data directory

--- assets/scripts/layers.js
+++ assets/scripts/layers.js
@@ -1,4 +1,4 @@
-let DATA_DIRECTORY = 'data/';
+let DATA_DIRECTORY = './assets/data/';
 if (typeof LAUNCH_LOCATIONS_FILENAME === 'undefined' || LAUNCH_LOCATIONS_FILENAME === null) {
     var LAUNCH_LOCATIONS_FILENAME = '../launch_locations.geojson';
 }
